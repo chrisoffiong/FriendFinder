e25d4927b9763bb66061f5776cd64fa6a94ec786
Chris Offiong <chrisoffiong@gmail.com>
2019-02-25 19:33:27 -0500

    revised

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,18 @@
-let arrData = []
+let arrData = [	{
+    name: 'Kitten',
+    photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+    scores: [
+        '4',
+        '3',
+        '5',
+        '1',
+        '1',
+        '5',
+        '5',
+        '1',
+        '1',
+        '1'
+    ]
+}]
 
 module.exports = arrData
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ let PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.static(path.join(__dirname, './app/public')))
 
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
