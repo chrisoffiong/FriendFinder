d3b65f192b2a73d6c64de10a8493041052b62228
Chris Offiong <chrisoffiong@gmail.com>
2019-02-25 22:12:45 -0500

    revision

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,38 @@ let arrData = [	{
         '1',
         '1'
     ]
+},
+{
+    name: 'Lucas',
+    photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+    scores: [
+        '4',
+        '3',
+        '5',
+        '1',
+        '1',
+        '5',
+        '5',
+        '1',
+        '1',
+        '1'
+    ]
+},
+{
+    name: 'Cloe',
+    photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+    scores: [
+        '1',
+        '1',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '1'
+    ]
 }]
 
 module.exports = arrData
